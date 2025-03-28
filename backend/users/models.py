996fc65f013f6330a08ebc4fbdb56bcbfa000ece
MrWalu <wbartek37@wp.pl>
2025-03-28 17:28:30 +0100

    Creating admin account
    Models and configs needed to create admin account

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Administrator'),
+        ('user', 'User'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+
+    def is_admin(self):
+        return self.role == 'admin' or self.is_superuser
+
+    def is_user(self):
+        return self.role == 'user'
