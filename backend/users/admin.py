ff60c47ad8f746fc85e5e2c5922913490495a127
Mikołaj Skałka <skalkamikolaj@gmail.com>
2025-04-05 16:04:21 +0200

    Formatting

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,15 +3,15 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional info", {"fields": ("role",)}),
-    )
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("role",)}),)
     list_display = ("username", "email", "role", "is_staff", "is_superuser")
 
     def save_model(self, request, obj, form, change):
         if not request.user.is_superuser and obj.is_superuser:
             raise ValueError("Only a superuser can promote admins!")
         super().save_model(request, obj, form, change)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- backend/users/views.py
+++ backend/users/views.py
@@ -3,19 +3,22 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .forms import RegisterForm
 
+
 def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')  # Zmień na swoją stronę główną
+            return redirect("home")  # Zmień na swoją stronę główną
     else:
         form = RegisterForm()
     return render(request, "register.html", {"form": form})
 
+
 def is_admin(user):
     return user.is_authenticated and user.is_admin()
+
 
 @login_required
 @user_passes_test(is_admin)
